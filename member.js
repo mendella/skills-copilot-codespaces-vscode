ed9bcffc5ab5325b3b66084492d2d1ef3645e801
Bruno Mendella <mendella@gmail.com>
2023-07-24 17:53:40 -0300

    01

--- member.js
+++ member.js
@@ -1,1 +1,14 @@
-function skillsMember()
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberSkills = document.getElementById("memberSkills");
+  var memberSkillsButton = document.getElementById("memberSkillsButton");
+  if (memberSkills.style.display === "none") {
+    memberSkills.style.display = "block";
+    memberSkillsButton.innerHTML = "Hide Skills";
+    member.style.height = "auto";
+  } else {
+    memberSkills.style.display = "none";
+    memberSkillsButton.innerHTML = "Show Skills";
+    member.style.height = "auto";
+  }
+}
